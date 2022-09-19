2561ad7eb29457b6ec94ac3768abba6121ea99ea
digarfi <digarfi@gmail.com>
2022-09-19 15:24:49 -0300

    Order Status Enum added

--- backend/src/main/java/com/digarfi/orderProduct/config/TestConfig.java
+++ backend/src/main/java/com/digarfi/orderProduct/config/TestConfig.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Profile;
 
 import com.digarfi.orderProduct.entities.Order;
 import com.digarfi.orderProduct.entities.User;
+import com.digarfi.orderProduct.entities.enums.OrderStatus;
 import com.digarfi.orderProduct.repositories.OrderRepository;
 import com.digarfi.orderProduct.repositories.UserRepository;
 
@@ -31,9 +32,9 @@ public class TestConfig implements CommandLineRunner{
 		User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456"); 
 		User u3 = new User(null,"Mara Oliveira", "mara@gmail.com", "984678765","123456");
 		
-		Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), u1); 
-		Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), u2); 
-		Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), u1); 
+		Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"),OrderStatus.PAID, u1); 
+		Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"),OrderStatus.WAITING_PAYMENT, u2); 
+		Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"),OrderStatus.WAITING_PAYMENT, u3); 
 		
 		
 		userRepository.saveAll(Arrays.asList(u1,u2,u3));

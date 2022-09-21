cd6f84340d3dfaa7f62deff09a4fd4b9cf68dba1
digarfi <digarfi@gmail.com>
2022-09-20 22:04:40 -0300

    Exception Handling findById added

--- backend/src/main/java/com/digarfi/orderProduct/services/UserService.java
+++ backend/src/main/java/com/digarfi/orderProduct/services/UserService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import com.digarfi.orderProduct.entities.User;
 import com.digarfi.orderProduct.repositories.UserRepository;
+import com.digarfi.orderProduct.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class UserService {
@@ -21,7 +22,7 @@ public class UserService {
 	
 	public User findById(Long id){
 		Optional <User> obj = repository.findById(id);
-		return obj.get();
+		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
 	}
 	
 	public User insert(User obj) {
---
+++ backend/src/main/java/com/digarfi/orderProduct/services/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,11 @@
+package com.digarfi.orderProduct.services.exceptions;
+
+public class ResourceNotFoundException extends RuntimeException{
+	private static final long serialVersionUID = 1L;
+	
+	public ResourceNotFoundException (Object id) {
+		super("Resource not find. Id" + id);
+	}
+	
+
+}
